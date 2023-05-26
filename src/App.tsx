7900acec2ee87f0662a62b41b7938273b5c93343
Kichan <rlcks01537@gmail.com>
2023-05-26 16:29:45 +0900

    TS interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,13 @@
 import styled from "styled-components";
+import Circle from "./Circle";
 
-const Title = styled.h1`
-  color: ${(props) => props.theme.textColor};
-`;
-
-const Wrapper = styled.div`
-  display: flex;
-  height: 100vh;
-  width: 100vw;
-  justify-content: center;
-  align-items: center;
-  background-color: ${(props) => props.theme.backgroundColor};
-`;
 
 function App() {
   return (
-    <Wrapper>
-      <Title>Dark Mode</Title>
-    </Wrapper>
+    <div>
+      <Circle bgColor="red" />
+      <Circle bgColor="blue" />
+    </div>
   );
 }
 export default App;
