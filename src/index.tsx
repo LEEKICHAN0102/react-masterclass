5a324dc005f5b5486a1deaadfb0ea2848990efef
Kichan <rlcks01537@gmail.com>
2023-05-28 14:43:58 +0900

    react-router-dom v5.2.0 if it issues fix pacakage.json --Coin API

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { ThemeProvider } from "styled-components";
-import { darkTheme, lightTheme } from "./theme";
+import {  theme } from "./theme";
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <ThemeProvider theme={darkTheme}>
+    <ThemeProvider theme={theme}>
       <App />
     </ThemeProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
+
+//https://styled-components.com/docs/api#typescript//
