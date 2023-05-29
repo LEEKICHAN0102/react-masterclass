49ba48f4b882415d226c56962fc549c9205642c4
Kichan <rlcks01537@gmail.com>
2023-05-29 16:38:59 +0900

    react-query replaced fecther function-> code more than clearly

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -5,6 +5,7 @@ import styled from "styled-components";
 import Chart from "./Chart";
 import Price from "./Price";
 
+
 const Container =styled.div`
   padding:0px 20px;
   max-width:480px;
--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
+import { useQuery } from "react-query";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
+import { fetchCoins } from "../api";
 
 const Container =styled.div`
   padding:0px 20px;
@@ -89,7 +91,7 @@ const coins=[
     }
   ];
 
-interface CoinInterface {
+interface ICoin {
   id: string,
   name: string,
   symbol: string,
@@ -101,23 +103,22 @@ interface CoinInterface {
 
 
 function Coins(){
-  const [coins,setCoins]=useState<CoinInterface[]>([]);
-  const [loading,setLoading]=useState(true);
-  useEffect(()=>{
-    (async()=>{
-      const response=await fetch("https://api.coinpaprika.com/v1/coins");
-      const json=await response.json();
-      setCoins(json.slice(0,100));
-      setLoading(false);
-    })();
-  },[]);
+  const {isLoading,data}=useQuery<ICoin[]>("allCoins", fetchCoins); //Replaced by react-query!!
+  // useEffect(()=>{
+  //   (async()=>{
+  //     const response=await fetch("https://api.coinpaprika.com/v1/coins");
+  //     const json=await response.json();
+  //     setCoins(json.slice(0,100));
+  //     setLoading(false);
+  //   })();
+  // },[]);
   return <Container>
     <Header>
       <Title>Coin API</Title>
     </Header>
-    {loading ? <Loader>코인 불러오는 중</Loader>:
+    {isLoading ? <Loader>코인 불러오는 중</Loader>:
     <CoinList>
-      {coins.map((coin) => (
+      {data?.slice(0,100).map((coin) => (
           <Coin key={coin.id}>
             <Link to={{
               pathname:`/${coin.id}`,
