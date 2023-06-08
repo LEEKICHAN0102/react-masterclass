52b5a0a9edfd7fccb838749cb733b670a452dd4b
Kichan <rlcks01537@gmail.com>
2023-06-08 15:50:27 +0900

    MotionValues Rendering

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
-import {useRef}from "react";
+import { useEffect } from "react";
 import styled from "styled-components";
-import {motion} from "framer-motion";
+import {motion,
+  useMotionValue,
+  useTransform,
+  useScroll,
+} from "framer-motion";
 
-const Wrapper = styled.div`
-  
-  height: 100vh;
-  width: 100vw;
-  
+const Wrapper = styled(motion.div)`
+  height:200vh;
+  width: 100vw;  
   display: flex;
   justify-content: center;
   align-items: center;
+  background-color:linear-gradient(135deg,rgb(238,0,153),rgb(221, 0, 238));
   
 `;
 
@@ -50,20 +53,19 @@ const boxVariants={
 
 
 function App() {
-  const biggerBoxRef=useRef<HTMLDivElement>(null);
+  const x=useMotionValue(0);
+  const rotateZ=useTransform(x,[-800,800],[-360,360]);
+  const gradient=useTransform(x,[-800,800],
+    [
+      "linear-gradient(135deg, rgb(0, 210, 238), rgb(0, 83, 238))",
+      "linear-gradient(135deg, rgb(0, 238, 155), rgb(238, 178, 0))",
+    ]
+  );
+  const { scrollYProgress } = useScroll();
+  const scale = useTransform(scrollYProgress, [0, 1], [1, 5]);
   return (
-    <Wrapper>
-      <BiggerBox ref={biggerBoxRef}>
-      <Box 
-      drag 
-      dragSnapToOrigin
-      dragElastic={0}
-      dragConstraints={biggerBoxRef}
-      variants={boxVariants}
-      whileHover="hover" 
-      whileDrag="dragging" 
-      whileTap="click"/>
-    </BiggerBox>
+    <Wrapper style={{background:gradient}}>
+      <Box style={{x,rotateZ,scale}} drag="x" dragSnapToOrigin/>
     </Wrapper>
   );
 }
