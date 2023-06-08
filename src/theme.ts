462a323cd51f52a88227356270b31afb25a65031
Kichan <rlcks01537@gmail.com>
2023-06-08 12:35:47 +0900

    Animate

--- src/theme.ts
+++ src/theme.ts
@@ -1,18 +1,3 @@
 import { DefaultTheme } from "styled-components";
 
-export const darkTheme:DefaultTheme={
-  bgColor:"#2f3640",
-  textColor: "white",
-  accentColor: "#9c88ff",
-  cardBgColor: "transparent",
-}
-
-export const lightTheme:DefaultTheme={
-  bgColor: "whitesmoke",
-  textColor: "black",
-  accentColor: "#9c88ff",
-  cardBgColor: "white",
-}
-
-//https://codesandbox.io/s/intelligent-darkness-r61jxu
-//I spend all day this challenge... ToT
+export const darkTheme: DefaultTheme = {};
