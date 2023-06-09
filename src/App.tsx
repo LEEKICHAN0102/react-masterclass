b1fed0a987d7955514e32b0fc2f106344717e729
Kichan <rlcks01537@gmail.com>
2023-06-09 11:30:47 +0900

    Animate Presence cumtom

--- src/App.tsx
+++ src/App.tsx
@@ -26,12 +26,14 @@ const Box = styled(motion.div)`
 `;
 
 const box = {
-  invisible: {
-    x: 500,
-    opacity: 0,
-    scale: 0,
+  entry: (back:boolean)=>{
+    return {
+      x: back?-500:500,
+      opacity:0,
+      scale:0
+    }
   },
-  visible: {
+  center: {
     x: 0,
     opacity: 1,
     scale: 1,
@@ -39,29 +41,43 @@ const box = {
       duration: 1,
     },
   },
-  exit: { x: -500, opacity: 0, scale: 0, transition: { duration: 1 } },
+  exit:(back:boolean)=>{
+    return {
+      x: back ? 500:-500,
+      opacity:0,
+      scale:0,
+      transition:{
+        duration:1,
+      }
+    }
+  },
 };
+
 
 function App() {
   const [visible, setVisible] = useState(1);
-  const nextPlease = () => setVisible((prev) => (prev === 10 ? 10 : prev + 1));
-  const prevPlease = () => setVisible((prev) => (prev === 1 ? 1 : prev - 1));
+  const [back,setBack]=useState(false);
+  const nextPlease = () => {
+    setBack(false);
+    setVisible((prev) => (prev === 10 ? 10 : prev + 1));
+  }
+  const prevPlease = () => {
+    setBack(true);
+    setVisible((prev) => (prev === 1 ? 1 : prev - 1));
+  }
   return (
     <Wrapper> 
-      <AnimatePresence>
-        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((i) =>
-          i === visible ? (
+      <AnimatePresence custom={back}>
             <Box
+              custom={back}
               variants={box}
-              initial="invisible"
-              animate="visible"
+              initial="entry"
+              animate="center"
               exit="exit"
-              key={i}
+              key={visible}
             >
-              {i}
+              {visible}
             </Box>
-          ) : null
-        )}
       </AnimatePresence>
       <button onClick={nextPlease}>next</button>
       <button onClick={prevPlease}>prev</button>
