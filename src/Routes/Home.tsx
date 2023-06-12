323b4911a430c601e0eca711f35a868e4864e849
Kichan <rlcks01537@gmail.com>
2023-06-12 12:59:01 +0900

    NETFLIX slider

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -1,5 +1,7 @@
+import {useState} from "react";
 import styled from "styled-components";
 import {useQuery} from "react-query";
+import {motion,AnimatePresence} from "framer-motion";
 import {getMovies,IGetMoviesResult} from "../api";
 import { makeImagePath } from "../utils";
 
@@ -35,19 +37,65 @@ const OverView=styled.p`
   width:70%;
 `;
 
+const Slider=styled.div`
+  position:relative;
+  top:-100px;
+`;
 
+const Row=styled(motion.div)`
+  display:grid;
+  gap:10px;
+  grid-template-columns:repeat(6,1fr);
+  position:absolute;
+  width:100%;
+`;
+
+const rowVariants={
+  hidden:{
+    x:window.outerWidth+10,
+  },
+  visible:{
+    x:0,
+  },
+  exit:{
+    x:-window.outerWidth-10
+  },
+}
+
+const Box = styled(motion.div)`
+  background-color:white;
+  height:150px;
+  color:tomato;
+`;
 
 function Home () {
   const {data,isLoading}=useQuery<IGetMoviesResult>(["movies","nowPlaying"],getMovies);
-  
+  const [index,setIndex]=useState(0);
+  const increaseIndex=()=>setIndex((prev)=>prev+1);
   return (
     <Wrapper>
       {isLoading?<Loader>Loading...</Loader>:
       <>
-        <Banner bgPhoto={makeImagePath(data?.results[0].backdrop_path||"")}>
+        <Banner onClick={increaseIndex} bgPhoto={makeImagePath(data?.results[0].backdrop_path||"")}>
           <Title>{data?.results[0].title}</Title>
           <OverView>{data?.results[0].overview}</OverView>
         </Banner>
+        <Slider>
+          <AnimatePresence>
+            <Row 
+              variants={rowVariants}
+              initial="hidden"
+              animate="visible"
+              transition={{
+                type:"tween",
+                duration:1
+              }}
+              exit="exit"
+              key={index}>
+              {[1,2,3,4,5,6].map((i)=><Box key={i}>{i}</Box>)}
+            </Row>
+          </AnimatePresence>
+        </Slider>
       </>}
     </Wrapper>
   )
